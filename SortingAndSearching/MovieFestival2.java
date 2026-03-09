d216ec3682be02e59b55529aee43860dcf9049d3
avina7061 <avinashchy73@gmail.com>
2026-03-09 21:48:34 +0530

    new Update

--- SortingAndSearching/MovieFestival2.java
+++ SortingAndSearching/MovieFestival2.java
@@ -1,45 +1,52 @@
-package CSES_Problem_SET.SortingAndSearching;
+//package CSES_Problem_SET.SortingAndSearching;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.io.*;
+import java.util.*;
 
 public class MovieFestival2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         int dp[][] = new int[n][2];
 
         for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             dp[i][0] = a;
             dp[i][1] = b;
         }
 
         Arrays.sort(dp, (a, b) -> Integer.compare(a[1], b[1]));
 
-        TreeMap<Integer, Integer> map = new TreeMap<>();// we have to store all end
+        TreeMap<Integer, Integer> map = new TreeMap<>();
 
         for (int i = 0; i < k; i++) {
             map.put(0, map.getOrDefault(0, 0) + 1);
         }
+
         int ans = 0;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int start = dp[i][0];
             int end = dp[i][1];
 
             Integer key = map.floorKey(start);
+
             if (key != null) {
-                map.put(key, map.getOrDefault(key, 0) - 1);
+                map.put(key, map.get(key) - 1);
                 if (map.get(key) == 0) map.remove(key);
+
                 map.put(end, map.getOrDefault(end, 0) + 1);
                 ans++;
             }
         }
+
         System.out.println(ans);
     }
 }
